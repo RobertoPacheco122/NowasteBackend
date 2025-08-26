4e06ba74c9854bd6f44fe51d644098c114abacee
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2025-08-26 09:58:41 -0300

    Add project files.

---
+++ src/Nowaste.Api/Controllers/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nowaste.Api.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+    }
+}
---
+++ src/Nowaste.Communication/Requests/Users/RequestRegisterUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Nowaste.Communication.Requests.Users;
+
+public class RequestRegisterUser {
+
+}
+
